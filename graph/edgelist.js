a2683111e64795090d8a7c453ebf871f415914da
Saish Mungase <saishmungase@gmail.com>
2026-01-09 17:35:35 +0530

    [GRAPH] converter :: edgelist to adjency list

---
+++ graph/edgelist.js
@@ -0,0 +1,16 @@
+// edgelist -> adjencylist
+
+export const elToAl = (edges) => {
+    const graph = {}
+    for(let edge of edges){
+        const [a, b] = edge;
+        if(!(a in graph)){
+            graph[a] = [];
+        }
+        if(!(b in graph)) graph[b] = [];
+
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+    return graph;
+}
